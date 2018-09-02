93000c6a6fcb03e084183148d337a8fb716f2806
douglasliralima <douglasliralima@gmail.com>
2018-09-02 20:47:29 -0300

    v0.2.0 - Front com back

--- CalculoNumerico/Raizes/views.py
+++ CalculoNumerico/Raizes/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def paginaInicial(request):
+	print(request)
+	if request.method == "POST" and 'falsaPosicaoSalvar' in request.POST:
+		print(request.POST)
+		equacao = request.POST.get('falsaPosicaoEquacaoInput')
+		LI = request.POST.get("falsaPosicaoLIInput")
+		LU = request.POST.get("falsaPosicaoLSInput")
+		tolerancia = request.POST.get("falsaPosicaoToleranciaInput")
+		print(
+			'equacao:' , equacao, '\n',
+			'Input:' , equacao, '\n',
+			'equacao:' , equacao, '\n',
+			'equacao:' , equacao, '\n'
+			)
 	contexto = {}
 	return render(request, "Raizes/base.html", contexto)
