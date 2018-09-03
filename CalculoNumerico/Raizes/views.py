79a10e1a48ac8707d7469bf6e2abf5179d233392
douglasliralima <douglasliralima@gmail.com>
2018-09-02 21:26:24 -0300

    v0.2.1 - front com back quase completo

--- CalculoNumerico/Raizes/views.py
+++ CalculoNumerico/Raizes/views.py
@@ -11,9 +11,35 @@ def paginaInicial(request):
 		tolerancia = request.POST.get("falsaPosicaoToleranciaInput")
 		print(
 			'equacao:' , equacao, '\n',
-			'Input:' , equacao, '\n',
-			'equacao:' , equacao, '\n',
-			'equacao:' , equacao, '\n'
+			'Limite Inferior:' , LI, '\n',
+			'Limte Superior:' , LU, '\n',
+			'tolerancia:' , tolerancia, '\n'
 			)
+	elif request.method == "POST" and 'secanteSalvar' in request.POST:
+		print(request.POST)
+		equacao = request.POST.get('secanteEquacaoInput')
+		x0 = request.POST.get("secanteX0Input")
+		x1 = request.POST.get("secanteX1Input")
+		tolerancia = request.POST.get("falsaPosicaoToleranciaInput")
+		print(
+			'equacao:' , equacao, '\n',
+			'x0:' , x0, '\n',
+			'x1:' , x1, '\n',
+			'tolerancia:' , tolerancia, '\n'
+			)
+	elif request.method == "POST" and 'mullerSalvar' in request.POST:
+		equacao = request.POST.get('mullerEquacaoInput')
+		x0 = request.POST.get("mullerX0Input")
+		x1 = request.POST.get("mullerX1Input")
+		x2 = request.POST.get("mullerX2Input")
+		tolerancia = request.POST.get("mullerToleranciaInput")
+		print(
+			'equacao:' , equacao, '\n',
+			'x0:' , x0, '\n',
+			'x1:' , x1, '\n',
+			'x2:' , x2, '\n',
+			'tolerancia:' , tolerancia, '\n'
+			)
+
 	contexto = {}
 	return render(request, "Raizes/base.html", contexto)
